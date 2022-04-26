7555e7f6e382b728882b084e5c39c79df74a81a6
Sahil Budhwar <sahilbudhwar143@gmail.com>
2022-04-26 22:31:22 +0530

    support new spi api for OAuth flow

--- src/components/AddComponent/__tests__/utils.spec.ts
+++ src/components/AddComponent/__tests__/utils.spec.ts
@@ -1,7 +1,10 @@
+import { commonFetch } from '@openshift/dynamic-plugin-sdk-utils';
+import { waitFor } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
 import { useK8sWatchResource } from '../../../dynamic-plugin-sdk';
 import { createComponentDetectionQuery } from '../../../utils/create-utils';
-import { useComponentDetection } from '../utils';
+import { useAccessTokenBindingAuth, useComponentDetection } from '../utils';
+import '@testing-library/jest-dom';
 
 jest.mock('../../../dynamic-plugin-sdk', () => ({
   useK8sWatchResource: jest.fn(),
@@ -11,8 +14,17 @@ jest.mock('../../../utils/create-utils', () => ({
   createComponentDetectionQuery: jest.fn(),
 }));
 
+jest.mock('@openshift/dynamic-plugin-sdk-utils', () => ({
+  commonFetch: jest.fn(),
+}));
+
+jest.mock('formik', () => ({
+  useFormikContext: () => ({ setFieldValue: jest.fn() }),
+}));
+
 const useK8sWatchMock = useK8sWatchResource as jest.Mock;
 const createCDQMock = createComponentDetectionQuery as jest.Mock;
+const commonFetchMock = commonFetch as jest.Mock;
 
 describe('useComponentDetection', () => {
   afterEach(jest.resetAllMocks);
@@ -51,5 +63,35 @@ describe('useComponentDetection', () => {
     renderHook(() => useComponentDetection('', 'test-app', 'test-ns'));
     expect(useK8sWatchMock).toHaveBeenCalledWith({});
     expect(createCDQMock).toHaveBeenCalledTimes(0);
+  });
+});
+
+describe('useAccessTokenBindingAuth', () => {
+  it('should call commonFetch with github auth url', async () => {
+    useK8sWatchMock.mockReturnValue([
+      { status: { oAuthUrl: 'https://xyz.com/github/authenticate?state=dummy-token' } },
+      true,
+      null,
+    ]);
+
+    renderHook(() => useAccessTokenBindingAuth('test-app'));
+
+    await waitFor(() =>
+      expect(commonFetchMock).toHaveBeenCalledWith('/auth/github/authenticate?state=dummy-token'),
+    );
+  });
+
+  it('should call commonFetch with quay auth url', async () => {
+    useK8sWatchMock.mockReturnValue([
+      { status: { oAuthUrl: 'https://xyz.com/quay/authenticate?state=dummy-token' } },
+      true,
+      null,
+    ]);
+
+    renderHook(() => useAccessTokenBindingAuth('test-app'));
+
+    await waitFor(() =>
+      expect(commonFetchMock).toHaveBeenCalledWith('/auth/quay/authenticate?state=dummy-token'),
+    );
   });
 });
--- src/components/AddComponent/utils.ts
+++ src/components/AddComponent/utils.ts
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { commonFetch } from '@openshift/dynamic-plugin-sdk-utils';
 import { useFormikContext } from 'formik';
 import { useK8sWatchResource } from '../../dynamic-plugin-sdk';
 import {
@@ -31,7 +32,49 @@ export const useAccessTokenBindingAuth = (name: string) => {
     if (!name || !loaded) return;
     const oAuthUrl = binding.status?.oAuthUrl;
     if (oAuthUrl) {
-      window.open(oAuthUrl);
+      const urlParams = new URLSearchParams(oAuthUrl.split('?')[1]);
+      const spType = oAuthUrl.includes('/github/') ? 'github' : 'quay';
+      commonFetch(`/auth/${spType}/authenticate?state=${urlParams.get('state')}`)
+        .then((response) => response.body)
+        // copied from https://developer.mozilla.org/en-US/docs/Web/API/ReadableStream
+        .then((rb: ReadableStream) => {
+          const reader = rb.getReader();
+
+          return new ReadableStream({
+            start(controller) {
+              // The following function handles each data chunk
+              function push() {
+                // "done" is a Boolean and value a "Uint8Array"
+                reader.read().then(({ done, value }) => {
+                  // If there is no more data to read
+                  if (done) {
+                    controller.close();
+                    return;
+                  }
+                  // Get the data and send it to the browser via the controller
+                  controller.enqueue(value);
+                  push();
+                });
+              }
+
+              push();
+            },
+          });
+        })
+        .then((stream) => {
+          // Respond with our stream
+          return new Response(stream, { headers: { 'Content-Type': 'text/html' } }).text();
+        })
+        .then((result) => {
+          const authorizeWindow = window.open('about:blank');
+          authorizeWindow.document.open();
+          authorizeWindow.document.write(result);
+          authorizeWindow.document.close();
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log('Error in oAuth url request', error);
+        });
     }
   }, [binding?.status?.oAuthUrl, loaded, name]);
 
